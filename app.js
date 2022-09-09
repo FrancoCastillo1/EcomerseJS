cdbaa98c724fca365c87478a0ed15e27112a9392
Franco Castillo <casfranco937@gmail.com>
2022-09-08 22:37:53 -0300

    1ra pre-entrega

--- app.js
+++ app.js
@@ -1,143 +1,117 @@
+/* Array de Cryptos */
+/* let criptoMonedas = []; */
+
 //funciones conversor divisas
-class Cripto{
-    constructor(nombre ,precio){
-        this.nombre = nombre;
-        this.precio = precio
-    }
+class Cripto {
+  constructor(nombre, precio , disponibilidad) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponibilidad = disponibilidad;
+  }
 }
-const cripto1 = new Cripto("Bitcoin" , 2700);
-const cripto2 = new Cripto("Lemon" , 345)
-const cripto3 = new Cripto("Cardano" , 1)
-const cripto4 = new Cripto("Dogecoin" , 1.1)
-let criptoMonedas =[
-    {TipoDeCripto:  "Bitcoin" , precio : cripto1.precio , disponibilidad :true},
-    {TipoDeCripto: /* cripto2.nombre */ "Lemon" , precio :cripto2.precio , disponibilidad: false},  
-    {TipoDeCripto: /* cripto3.nombre */ "Cardano", precio : cripto3.precio , disponibilidad :true},
-    {TipoDeCripto: /* cripto4.nombre */ "DogeCoin" , precio :cripto4.precio , disponibilidad :true}, 
-]
-function iniciarCompra(){
-     nombreUsuario = prompt("Cual es tu nombre");
-    alert(`Saludos ${nombreUsuario} bienvenido a la tienda DevanCoin`);
-    montoUsuario = Number(prompt("Ingrese su monto estimado, en USD o Cripto"))
+const cripto1 = new Cripto("Bitcoin", 2700 , true);
+const cripto2 = new Cripto("Lemon", 345 , true);
+const cripto3 = new Cripto("Cardano", 1 , false);
+const cripto4 = new Cripto("Dogecoin", 1.1 , true);
+
+// creo un array de objetos para que funciuone le find , lo que pedia el desafio
+let criptoMonedas = [
+  { TipoDeCripto: cripto1.nombre, precio: cripto1.precio, disponibilidad: cripto1.disponibilidad },
+  {TipoDeCripto: cripto2.nombre, precio: cripto2.precio,  disponibilidad: cripto2.disponibilidad,
+  },
+  { TipoDeCripto: cripto3.nombre, precio: cripto3.precio, disponibilidad: cripto3.disponibilidad },
+  { TipoDeCripto: cripto4.nombre, precio: cripto4.precio, disponibilidad: cripto4.disponibilidad },
+];
+/* Sirver para ver si le llego la información al Array */
+console.log(criptoMonedas);
+
+function iniciarCompra() {
+  nombreUsuario = prompt("Cual es tu nombre");
+  alert(`Saludos ${nombreUsuario} bienvenido a la tienda DevanCoin`);
 }
-function resumirCodeDivicion(moneda,cripto){
-     Number(prompt(`Has elegido ${cripto} , elegi cuanto en usd queres vender`));
-        let resultadoFinal = ventaCode /moneda
-        alert(`Usted a vendido ${resultadoFinal} de ${cripto}`)
-        datosUsuario.push(resultadoFinal)   
-}
-function resumirCodeCompra(moneda,cripto){
-     Number(prompt(`Has elegido ${cripto} , elegi cuanto en usd queres vender`));
-        let resultadoFinal = comprarCode * moneda
-        alert(`Usted a comprado ${resultadoFinal} de ${cripto}`)
-        datosUsuario.push(resultadoFinal)   
-}
-function compraVenta(){
-     switch(producto){
-          case "vender":
-           ventaCode = prompt(`Ingrese la divisa que desea vender , valor US
+function compraVenta() {
+  switch (producto) {
+    case "vender":
+      ventaCode = prompt(`Ingrese la divisa que desea vender , valor US
             1  bitcoins
             2  Lemon Cash
             3 Cardano
             4  Dogecoin
             Esc para salir del algoritmo
-            `)
-            while(ventaCode !=="esc"){   
-                 if(ventaCode ==="1"){
-                    resumirCodeDivicion(cripto1.precio ,"bitcoin")
-                    break
-                }
-                else if(ventaCode ==="2"){
-                    resumirCodeDivicion(cripto2.precio ,"Lemon")
-                    break
-                }
-                else if(ventaCode ==="3"){
-                    resumirCodeDivicion(cripto3.precio ,"Cardano")
-                    break
-                }
-                else if(ventaCode ==="4"){
-                    resumirCodeDivicion(cripto4.precio ,"Dogecoin")
-                    break
-                }else{
-                    alert("Por favor vuelva a ingresar un numero")
-                }
-                break
-            }
-        case "comprar":
-            comprarCode = prompt(`Ingrese la divisa que quiera comprar , valor US
+            `);
+          let recorrido = criptoMonedas.find(
+              (cripto) => cripto.TipoDeCripto === ventaCode
+              );
+              if(recorrido.disponibilidad == true){
+            let cantidad = Number(prompt(`Gracias por seleccionar ${recorrido.TipoDeCripto} con un precio de US$${recorrido.precio}
+            Por favor ingrese la cantidad de cripto exacta para concretar la venta
+              `));
+             let operacionFinal = cantidad * recorrido.precio
+                datosUsuario.push(operacionFinal)
+                alert(`Se vendieron de ${recorrido.TipoDeCripto} $US ${operacionFinal}`)
+              }else{
+                alert(`No esta disponible`)
+              }
+      break;
+    case "comprar":
+      comprarCode = prompt(`Ingrese la cripto moneda que desea comprar
             1:bitcoins
             2:Lemon Cash
             3:Cardano
             4:Dogecoin
             Esc para salir del algoritmo
-           `)
-           while(comprarCode !== "esc"){
-               if(comprar == "1"){
-                resumirCodeCompra(cripto1.precio ,"Bitcoin")
-                  break
-               }
-               else if(comprar == "2"){
-                resumirCodeCompra(cripto2.precio ,"Lemon")
-                 break
-               }
-               else if(comprar == "3"){
-                resumirCodeCompra(cripto3.precio ,"Cardano")
-                 break
-               }
-               else if(comprar == "4"){
-                resumirCodeCompra(cripto4.precio ,"Dogecoin")
-                break
-               }else{
-                   alert("Seleccione un numero valido ")
-               }
-           }
-           break
-        case "esc" || "salir":
-            alert("No comprara nada")
-        break
-     }
-    }
-//ersto lo usare mas adelante
-class UsuarioCoin{
-    constructor(nombre , ahorroCripto , vendido , comprado ){
-        this.nombre = nombre;
-        this.ahorroCripto = ahorroCripto
-        this.vendido = vendido;
-        this.comprado = comprado
-    }
-    porcentajeGancias(){
-        let ganado = (this.vendido - this.comprado) / this.vendido
-        return ganado
-    }
+           `);
+        let recorrido2 = criptoMonedas.find(
+          (cripto) => cripto.TipoDeCripto === comprarCode
+          );
+          if(recorrido2.disponibilidad == true){
+        let cantidad = Number(prompt(`Gracias por seleccionar ${recorrido2.TipoDeCripto} con un precio de US$${recorrido2.precio}
+        Por favor ingrese la cantidad de cripto exacta para concretar la compra
+          `));
+         let operacionFinal = cantidad / recorrido2.precio
+            datosUsuario.push(operacionFinal)
+            alert(`Se compraron de ${recorrido2.TipoDeCripto} $US ${operacionFinal}`)
+          }else{
+            alert(`No esta disponible`)
+          }
+      break;
+    case "esc" || "salir":
+      alert("No comprara nada");
+      break;
+  }
 }
-const USUARIO1 = new UsuarioCoin("Franco" , "5673" , "3479" , "5786");
-const USUARIO2 = new UsuarioCoin("Franco" , "56453" , "3479" , "5786");
- console.log(USUARIO1.porcentajeGancias())
+
+//esto lo usare mas adelante
+class UsuarioCoin {
+  constructor(nombre, ahorroCripto, vendido, comprado) {
+    this.nombre = nombre;
+    this.ahorroCripto = ahorroCripto;
+    this.vendido = vendido;
+    this.comprado = comprado;
+  }
+  porcentajeGancias() {
+    let ganado = (this.vendido - this.comprado) / this.vendido;
+    return ganado;
+  }
+}
+const USUARIO1 = new UsuarioCoin("Franco", 5673, 3479, 5786);/* A los numero, siempre declararlas como tipo number para realizar operaciones en un futuro */
+const USUARIO2 = new UsuarioCoin("Franco", 56453, 3479, 5786);
+console.log(USUARIO1.porcentajeGancias());
 //main
 let ventaCode;
 let comprarCode;
-let montoUsuario;
-let whilePadre = true
 let producto;
-let datosUsuario = []
+let datosUsuario = [];
 let nombreUsuario;
-producto = prompt(`Quieres comprar/vender criptos o salir `)
-while(producto != "esc" && whilePadre){
-    iniciarCompra()
-    compraVenta()
- let seguirComprando = prompt(`Desea seguir comprando/vendiendo`)
-    if(seguirComprando ==="no"){
-        alert(`Gracias por comercializar con nosotros ${nombreUsuario}`)
-        break
-    }else if(seguirComprando ==="si"){
-        while(seguirComprando !== "no" ){
-            producto = prompt(`bien , pon comprar o vender `)
-            compraVenta()
-            seguirComprando = prompt(`Desea seguir comprando/vendiendo xfd`)
-            if(seguirComprando =="no"){
-                whilePadre = false
-            }
-        }
-    }
- }
-alert(`Este fue le resultado de tu tu consulta ${datosUsuario.join(", ")} `)
+
+producto = prompt(`Quieres comprar/vender criptos o salir `);
+while (producto != "salir") {
+  iniciarCompra();
+  compraVenta();
+  let seguirComprando =confirm( `Desea seguir comprando/vendiendo. Ingrese no para salir`);
+  if (seguirComprando == false) {
+    alert(`Gracias por comercializar con nosotros ${nombreUsuario}`);
+    break;
+  } 
+}
+alert(`Este fue le resultado de tu tu consulta ${datosUsuario.join(", ")} `);
